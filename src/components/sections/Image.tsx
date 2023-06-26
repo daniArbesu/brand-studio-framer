46599b9593eb3790e42b65ad633b0558f537d208
daniArbesu <daniel.arbesu.sanchez@gmail.com>
2023-06-26 22:39:43 +0200

    feat: Image component

--- src/components/sections/Image.tsx
+++ src/components/sections/Image.tsx
@@ -1,5 +1,17 @@
-const Image = (): JSX.Element => {
-  return <div>Image</div>;
+interface Props {
+  src: string;
+  fallback: string;
+  type: string;
+  alt: string;
+}
+
+const Image = ({ src, fallback, type = 'image/webp', alt }: Props): JSX.Element => {
+  return (
+    <picture>
+      <source srcSet={src} type={type} />
+      <img src={fallback} alt={alt} />
+    </picture>
+  );
 };
 
 export default Image;
